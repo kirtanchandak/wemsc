3af5482f48ac2cdbce11d1edbe4c779729cf3f97
kirtanchandak <themarketingkirtan@gmail.com>
2024-08-02 18:20:13 +0530

    sidebar1 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Sidebar1 from "@/components/sidebar/Sidebar1";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
-    <div>
-      <h1>super kalam init!</h1>
-    </div>
+      <Sidebar1/>
    </>
   );
 }
